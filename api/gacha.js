04435b0443202d865368d46a52c983b950114d34
duanzhipeng59-dev <duanzhipeng59@gmail.com>
2025-11-10 16:24:09 +0800

    Update gacha.js

--- api/gacha.js
+++ api/gacha.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const prompt = "请告诉我，今天中午吃什么，不要重复，不要重复，每次发送的都必须不一样。";
+  const prompt = "请告诉我，今天中午吃什么，不要上次回复的一样，也不要和之前说的重复。";
   const apiKey = process.env.DEEPSEEK_API_KEY; // 从环境变量读取
 
   if (!apiKey) {
